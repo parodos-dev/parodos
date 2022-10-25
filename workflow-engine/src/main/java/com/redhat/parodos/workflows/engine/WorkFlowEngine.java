be8f853deaab195e38971633d381fbaa87214774
lshannon <luke.shannon@gmail.com>
2022-10-25 10:50:07 -0400

    First check-in of Workflow engine

---
+++ workflow-engine/src/main/java/com/redhat/parodos/workflows/engine/WorkFlowEngine.java
@@ -0,0 +1,46 @@
+/*
+ * The MIT License
+ *
+ *  Copyright (c) 2020, Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package com.redhat.parodos.workflows.engine;
+
+import com.redhat.parodos.workflows.work.WorkContext;
+import com.redhat.parodos.workflows.work.WorkReport;
+import com.redhat.parodos.workflows.workflow.WorkFlow;
+
+/**
+ * Interface for a workflow engine.
+ *
+ * @author Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
+ */
+public interface WorkFlowEngine {
+
+    /**
+     * Run the given workflow and return its report.
+     *
+     * @param workFlow to run
+     * @param workContext context in which the workflow will be run
+     * @return workflow report
+     */
+    WorkReport run(WorkFlow workFlow, WorkContext workContext);
+
+}
---
+++ workflow-engine/src/test/java/com/redhat/parodos/workflows/workflow/SequentialFlowTest.java
@@ -0,0 +1,89 @@
+/*
+ * The MIT License
+ *
+ *  Copyright (c) 2020, Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package com.redhat.parodos.workflows.workflow;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
+
+import com.redhat.parodos.workflows.work.Work;
+import com.redhat.parodos.workflows.work.WorkContext;
+
+public class SequentialFlowTest {
+
+    @Test
+    public void testExecute() {
+        // given
+        Work work1 = Mockito.mock(Work.class);
+        Work work2 = Mockito.mock(Work.class);
+        Work work3 = Mockito.mock(Work.class);
+        WorkContext workContext = Mockito.mock(WorkContext.class);
+        SequentialFlow sequentialFlow = SequentialFlow.Builder.aNewSequentialFlow()
+                .named("testFlow")
+                .execute(work1)
+                .then(work2)
+                .then(work3)
+                .build();
+
+        // when
+        sequentialFlow.execute(workContext);
+
+        // then
+        InOrder inOrder = Mockito.inOrder(work1, work2, work3);
+        inOrder.verify(work1, Mockito.times(1)).execute(workContext);
+        inOrder.verify(work2, Mockito.times(1)).execute(workContext);
+        inOrder.verify(work3, Mockito.times(1)).execute(workContext);
+    }
+
+    @Test
+    public void testPassingMultipleWorkUnitsAtOnce() {
+        // given
+        Work work1 = Mockito.mock(Work.class);
+        Work work2 = Mockito.mock(Work.class);
+        Work work3 = Mockito.mock(Work.class);
+        Work work4 = Mockito.mock(Work.class);
+        WorkContext workContext = Mockito.mock(WorkContext.class);
+        List<Work> initialWorkUnits = Arrays.asList(work1, work2);
+        List<Work> nextWorkUnits = Arrays.asList(work3, work4);
+        SequentialFlow sequentialFlow = SequentialFlow.Builder.aNewSequentialFlow()
+                .named("testFlow")
+                .execute(initialWorkUnits)
+                .then(nextWorkUnits)
+                .build();
+
+        // when
+        sequentialFlow.execute(workContext);
+
+        // then
+        InOrder inOrder = Mockito.inOrder(work1, work2, work3, work4);
+        inOrder.verify(work1, Mockito.times(1)).execute(workContext);
+        inOrder.verify(work2, Mockito.times(1)).execute(workContext);
+        inOrder.verify(work3, Mockito.times(1)).execute(workContext);
+        inOrder.verify(work4, Mockito.times(1)).execute(workContext);
+    }
+
+}
