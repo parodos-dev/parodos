aca4dd20bbbbd89c3593cc84a1f73061b17234fb
Annel Ketcha <annelketcha@annels-mbp.home>
2023-02-07 09:36:38 -0500

    remove unused libary

--- workflow-service/src/main/java/com/redhat/parodos/workflow/version/WorkFlowVersionServiceImpl.java
+++ workflow-service/src/main/java/com/redhat/parodos/workflow/version/WorkFlowVersionServiceImpl.java
@@ -1,18 +1,11 @@
 package com.redhat.parodos.workflow.version;
 
-import com.redhat.parodos.examples.simple.LoggingWorkFlowTaskExecution;
-import com.redhat.parodos.workflow.task.infrastructure.InfrastructureTaskDefinition;
-import com.redhat.parodos.workflow.task.parameter.WorkFlowTaskParameter;
-import com.redhat.parodos.workflow.task.parameter.WorkFlowTaskParameterType;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 
 @Service
 @Slf4j
