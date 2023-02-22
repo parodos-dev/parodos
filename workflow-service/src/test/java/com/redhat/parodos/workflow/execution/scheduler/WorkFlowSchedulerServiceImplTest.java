e435ddd6bd08151498e4aa5efe0a5abd0bb9d844
Eloy Coto <eloy.coto@acalustra.com>
2023-02-22 13:29:30 +0100

    Test: Add test for workflow Scheduler service
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

---
+++ workflow-service/src/test/java/com/redhat/parodos/workflow/execution/scheduler/WorkFlowSchedulerServiceImplTest.java
@@ -0,0 +1,91 @@
+package com.redhat.parodos.workflow.execution.scheduler;
+
+import com.redhat.parodos.workflows.work.Work;
+import com.redhat.parodos.workflows.work.WorkContext;
+import com.redhat.parodos.workflows.workflow.SequentialFlow;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.support.CronTrigger;
+
+import java.util.concurrent.ScheduledFuture;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class WorkFlowSchedulerServiceImplTest {
+
+	private TaskScheduler taskScheduler;
+
+	private WorkFlowSchedulerServiceImpl service;
+
+	private Work work;
+
+	private SequentialFlow workFlow;
+
+	@BeforeEach
+	public void eachInit() {
+		this.taskScheduler = Mockito.mock(TaskScheduler.class);
+		this.service = new WorkFlowSchedulerServiceImpl(taskScheduler);
+
+		this.work = Mockito.mock(Work.class);
+		this.workFlow = SequentialFlow.Builder.aNewSequentialFlow().named("test").execute(work).build();
+	}
+
+	@Test
+	void scheduleWithValidData() {
+		// given
+		Mockito.when(this.taskScheduler.schedule(Mockito.any(Runnable.class), Mockito.any(CronTrigger.class)))
+				.thenReturn(null);
+		// when
+		this.service.schedule(this.workFlow, new WorkContext(), "* * * * * *");
+		// then
+		Mockito.verify(this.taskScheduler, Mockito.times(1)).schedule(Mockito.any(Runnable.class),
+				Mockito.any(CronTrigger.class));
+	}
+
+	@Test
+	void workFlowIsNotScheduledTwice() {
+		// given
+		Mockito.when(this.taskScheduler.schedule(Mockito.any(Runnable.class), Mockito.any(CronTrigger.class)))
+				.thenReturn(null);
+		this.service.schedule(this.workFlow, new WorkContext(), "* * * * * *");
+
+		// when
+		this.service.schedule(this.workFlow, new WorkContext(), "* * * * * *");
+
+		// then
+		Mockito.verify(this.taskScheduler, Mockito.times(1)).schedule(Mockito.any(Runnable.class),
+				Mockito.any(CronTrigger.class));
+	}
+
+	@Test
+	void workFlowCanBeCancel() {
+		// given
+		ScheduledFuture mockScheduledFuture = Mockito.mock(ScheduledFuture.class);
+		Mockito.when(mockScheduledFuture.cancel(false)).thenReturn(true);
+		Mockito.when(this.taskScheduler.schedule(Mockito.any(Runnable.class), Mockito.any(CronTrigger.class)))
+				.thenReturn(mockScheduledFuture);
+		this.service.schedule(this.workFlow, new WorkContext(), "* * * * * *");
+
+		// when
+		this.service.stop(this.workFlow);
+
+		// then
+		Mockito.verify(mockScheduledFuture, Mockito.times(1)).cancel(Mockito.anyBoolean());
+	}
+
+	@Test
+	void workFlowIsNotCalledIfNoPresent() {
+		// given
+		ScheduledFuture mockScheduledFuture = Mockito.mock(ScheduledFuture.class);
+		Mockito.when(mockScheduledFuture.cancel(Mockito.anyBoolean())).thenReturn(true);
+		// when
+		boolean res = this.service.stop(this.workFlow);
+
+		// then
+		assertFalse(res);
+		Mockito.verify(mockScheduledFuture, Mockito.times(0)).cancel(Mockito.anyBoolean());
+	}
+
+}
