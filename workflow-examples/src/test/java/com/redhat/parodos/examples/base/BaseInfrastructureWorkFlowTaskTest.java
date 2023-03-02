17ed0a60e8e8fe5009465c61164c51ab84958cf7
Gloria Ciavarrini <gciavarrini@redhat.com>
2023-03-02 15:00:54 +0100

    Create BaseInfrastructureWorkFlowTaskTest
    This abstract class holds common setup for all classes that extends
    BaseInfrastructureWorkFlowTask abstract class
    
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>

---
+++ workflow-examples/src/test/java/com/redhat/parodos/examples/base/BaseInfrastructureWorkFlowTaskTest.java
@@ -0,0 +1,35 @@
+package com.redhat.parodos.examples.base;
+
+import com.redhat.parodos.workflow.task.WorkFlowTaskType;
+import com.redhat.parodos.workflow.task.infrastructure.BaseInfrastructureWorkFlowTask;
+import com.redhat.parodos.workflows.workflow.WorkFlow;
+import org.junit.Before;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.when;
+
+public abstract class BaseInfrastructureWorkFlowTaskTest {
+
+	protected final static String workflowTestName = "TestName";
+
+	private BaseInfrastructureWorkFlowTask baseInfrastructureWorkFlowTask;
+
+	@Before
+	public void setUp() {
+		this.baseInfrastructureWorkFlowTask = getConcretePersonImplementation();
+	}
+
+	/**
+	 * @return new instance of non-abstract class extending BaseInfrastructureWorkFlowTask
+	 */
+	protected abstract BaseInfrastructureWorkFlowTask getConcretePersonImplementation();
+
+	private WorkFlowTaskType type = WorkFlowTaskType.INFRASTRUCTURE;
+
+	public WorkFlow getWorkFlowChecker() {
+		WorkFlow testWorkflow = Mockito.mock(WorkFlow.class);
+		when(testWorkflow.getName()).thenReturn(workflowTestName);
+		return testWorkflow;
+	}
+
+}
