ee8ac7c7231069ef4bc315db5f1d77e63129575a
Gloria Ciavarrini <gciavarrini@redhat.com>
2023-03-03 14:28:25 +0100

    Create BaseWorkFlowCheckerTaskTest
    This abstract class holds common setup for all classes that extends
    BaseWorkFlowCheckerTask abstract class
    
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>

---
+++ workflow-examples/src/test/java/com/redhat/parodos/examples/base/BaseWorkFlowCheckerTaskTest.java
@@ -0,0 +1,33 @@
+package com.redhat.parodos.examples.base;
+
+import com.redhat.parodos.workflow.option.WorkFlowOption;
+import com.redhat.parodos.workflow.task.WorkFlowTaskType;
+import com.redhat.parodos.workflow.task.checker.BaseWorkFlowCheckerTask;
+import org.junit.Before;
+
+import java.util.List;
+
+public abstract class BaseWorkFlowCheckerTaskTest {
+
+	/**
+	 * These are the options this AssessmentTasks can return
+	 */
+	private List<WorkFlowOption> workflowOptions;
+
+	private BaseWorkFlowCheckerTask baseWorkFlowCheckerTask;
+
+	@Before
+	public void setUp() {
+		this.baseWorkFlowCheckerTask = getConcretePersonImplementation();
+	}
+
+	/**
+	 * @return new instance of non-abstract class extending BaseWorkFlowCheckerTaskTest
+	 */
+	protected abstract BaseWorkFlowCheckerTask getConcretePersonImplementation();
+
+	public WorkFlowTaskType getType() {
+		return WorkFlowTaskType.CHECKER;
+	}
+
+}
