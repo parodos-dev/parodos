9cb636cdbf71c595d6fa38eff2ff715812a20dd0
Gloria Ciavarrini <gciavarrini@redhat.com>
2023-03-06 16:50:56 +0100

    Fix: INPUT constant must be private
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>

--- workflow-examples/src/main/java/com/redhat/parodos/examples/continued/complex/OnboardingAssessmentTask.java
+++ workflow-examples/src/main/java/com/redhat/parodos/examples/continued/complex/OnboardingAssessmentTask.java
@@ -37,7 +37,7 @@ import java.util.List;
  */
 public class OnboardingAssessmentTask extends BaseAssessmentTask {
 
-	static final String INPUT = "INPUT";
+	private static final String INPUT = "INPUT";
 
 	public OnboardingAssessmentTask(WorkFlowOption workflowOption) {
 		super(List.of(workflowOption));
