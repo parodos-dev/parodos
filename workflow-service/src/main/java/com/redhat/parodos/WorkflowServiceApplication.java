c4162d05d53b184918b796dafaae8a354c25a45b
Moti Asayag <masayag@redhat.com>
2023-03-20 13:12:17 +0200

    Remove unused annotation
    The ComponentScan annotation is implicitly declared by SpringBootApplication
    annotation.
    
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- workflow-service/src/main/java/com/redhat/parodos/WorkflowServiceApplication.java
+++ workflow-service/src/main/java/com/redhat/parodos/WorkflowServiceApplication.java
@@ -19,7 +19,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
