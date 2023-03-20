4b5bfdbbfee7cd2473af9e9481163fc10829503f
Moti Asayag <masayag@redhat.com>
2023-03-20 21:31:52 +0200

    Decouple examples from service
    The purpose of this PR is to decouple the user's workflows from being
    built into the workflow-service by using the PropertiesLauncher as the
    Start-Class of the application.
    
    Extra dependencies can be provided via system property `loader.path`.
    In addition, to satisfy all runtime dependencies, the workflow-example
    is being built with a plugin to include all of its dependencies in a
    single jar.
    
    For local development, a local profile was added to include the
    workflow-examples dependency in the service's path:
    
    ```
    mvn clean install -Plocal
    ```
    
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- workflow-service/start_workflow_service.sh
+++ workflow-service/start_workflow_service.sh
@@ -1,1 +1,3 @@
-java -jar -Dspring.profiles.active=local target/workflow-service-1.0.3-SNAPSHOT.jar
+java -jar -Dspring.profiles.active=local \
+    -Dloader.path=../workflow-examples/target/workflow-examples-1.0.3-SNAPSHOT-jar-with-dependencies.jar \
+    target/workflow-service-1.0.3-SNAPSHOT.jar
