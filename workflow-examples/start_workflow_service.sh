1546be4bda05445cd49457cf7057bb56532de972
Marek Libra <marek.libra@gmail.com>
2023-03-23 12:46:26 +0100

    Add start_notification_service.sh

--- workflow-examples/start_workflow_service.sh
+++ workflow-examples/start_workflow_service.sh
@@ -1,3 +1,5 @@
+export SERVER_PORT=8080
+
 java -jar -Dspring.profiles.active=local \
     -Dloader.path=../workflow-examples/target/workflow-examples-1.0.5-SNAPSHOT-jar-with-dependencies.jar \
     ../workflow-service/target/workflow-service-1.0.5-SNAPSHOT.jar
