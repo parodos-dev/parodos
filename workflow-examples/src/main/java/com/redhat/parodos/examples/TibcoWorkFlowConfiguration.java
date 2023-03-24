cdf2c6073e5acc4ef489b6d2b6714b9cb15f3c13
lshannon <luke.shannon@gmail.com>
2023-03-24 14:26:48 -0400

    Adding a profile to enable 'Tibco' task only under a specific profile

--- workflow-examples/src/main/java/com/redhat/parodos/examples/TibcoWorkFlowConfiguration.java
+++ workflow-examples/src/main/java/com/redhat/parodos/examples/TibcoWorkFlowConfiguration.java
@@ -1,12 +1,11 @@
 // @formatter:off
 package com.redhat.parodos.examples;
 
-import javax.jms.JMSException;
-
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import com.redhat.parodos.tasks.tibco.TibcoJmsServiceImpl;
 import com.redhat.parodos.tasks.tibco.TibcoMessageService;
@@ -22,6 +21,7 @@ import com.tibco.tibjms.TibjmsConnectionFactory;
  *
  */
 @Configuration
+@Profile("tibco")
 public class TibcoWorkFlowConfiguration {
 	
 	@Bean
@@ -35,7 +35,7 @@ public class TibcoWorkFlowConfiguration {
 	// Add the hostname 'server' to 127.0.0.1 entry in /etc/hosts
 	// Set the admin password in TIBCO to 'admin' or edit the username and password below
 	@Bean
-	TibcoMessageService tibcoMessageService(TibjmsConnectionFactory tibjmsConnectionFactory, @Value("${caFile:/opt/tibco/ems/10.2/samples/certs/server_root.cert.pem}") String caFile, @Value("${username:admin}") String username, @Value("${password:admin}") String password) throws JMSException {
+	TibcoMessageService tibcoMessageService(TibjmsConnectionFactory tibjmsConnectionFactory, @Value("${caFile:/opt/tibco/ems/10.2/samples/certs/server_root.cert.pem}") String caFile, @Value("${username:admin}") String username, @Value("${password:admin}") String password) {
 		return new TibcoJmsServiceImpl(tibjmsConnectionFactory, caFile, username, password);
 	}
 
@@ -46,7 +46,7 @@ public class TibcoWorkFlowConfiguration {
 
 	@Bean
 	@Infrastructure
-	WorkFlow tibcoWorkFlow(@Qualifier("tibcoTask") TibcoWorkFlowTask tibcoTask) {
+	WorkFlow tibcoWorkFlow(TibcoWorkFlowTask tibcoTask) {
 		// @formatter:off
 		return SequentialFlow.Builder
 				.aNewSequentialFlow()
