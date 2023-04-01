055c5cb959e9621a2d7adc28499c1e8c018d393a
Annel Ketcha <annelketcha@gmail.com>
2023-04-01 12:05:52 -0400

    clean up

--- workflow-examples/src/main/java/com/redhat/parodos/examples/ocponboarding/task/AppLinkEmailNotificationWorkFlowTask.java
+++ workflow-examples/src/main/java/com/redhat/parodos/examples/ocponboarding/task/AppLinkEmailNotificationWorkFlowTask.java
@@ -18,8 +18,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.Collections;
