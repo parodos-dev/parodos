1edd224f9a7636a3e52efd107558824dd2fe13dc
Nir Argaman <nargaman@redhat.com>
2023-04-02 12:42:40 +0300

    SequentialFlow: Skip next work units if last workReport is empty or with "not completed" status

--- workflow-engine/src/test/java/com/redhat/parodos/workflows/workflow/SequentialFlowTest.java
+++ workflow-engine/src/test/java/com/redhat/parodos/workflows/workflow/SequentialFlowTest.java
@@ -26,57 +26,127 @@ package com.redhat.parodos.workflows.workflow;
 import java.util.Arrays;
 import java.util.List;
 
+import com.redhat.parodos.workflows.work.*;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InOrder;
 import org.mockito.Mockito;
 
-import com.redhat.parodos.workflows.work.Work;
-import com.redhat.parodos.workflows.work.WorkContext;
-
 public class SequentialFlowTest {
 
-	@Test
-	public void testExecute() {
-		// given
-		Work work1 = Mockito.mock(Work.class);
-		Work work2 = Mockito.mock(Work.class);
-		Work work3 = Mockito.mock(Work.class);
-		WorkContext workContext = Mockito.mock(WorkContext.class);
-		SequentialFlow sequentialFlow = SequentialFlow.Builder.aNewSequentialFlow().named("testFlow").execute(work1)
-				.then(work2).then(work3).build();
+	private final WorkContext workContext = Mockito.mock(WorkContext.class);
 
-		// when
-		sequentialFlow.execute(workContext);
+	private final WorkReport completedWorkReport = new DefaultWorkReport(WorkStatus.COMPLETED, workContext);
 
-		// then
-		InOrder inOrder = Mockito.inOrder(work1, work2, work3);
-		inOrder.verify(work1, Mockito.times(1)).execute(workContext);
-		inOrder.verify(work2, Mockito.times(1)).execute(workContext);
-		inOrder.verify(work3, Mockito.times(1)).execute(workContext);
+	private final WorkReport failedWorkReport = new DefaultWorkReport(WorkStatus.FAILED, workContext);
+
+	private final WorkReport progressWorkReport = new DefaultWorkReport(WorkStatus.IN_PROGRESS, workContext);
+
+	private Work[] works;
+
+	@Before
+	public void initWorks() {
+		works = new Work[] { Mockito.mock(Work.class), Mockito.mock(Work.class), Mockito.mock(Work.class),
+				Mockito.mock(Work.class) };
 	}
 
 	@Test
-	public void testPassingMultipleWorkUnitsAtOnce() {
-		// given
-		Work work1 = Mockito.mock(Work.class);
-		Work work2 = Mockito.mock(Work.class);
-		Work work3 = Mockito.mock(Work.class);
-		Work work4 = Mockito.mock(Work.class);
-		WorkContext workContext = Mockito.mock(WorkContext.class);
-		List<Work> initialWorkUnits = Arrays.asList(work1, work2);
-		List<Work> nextWorkUnits = Arrays.asList(work3, work4);
-		SequentialFlow sequentialFlow = SequentialFlow.Builder.aNewSequentialFlow().named("testFlow")
-				.execute(initialWorkUnits).then(nextWorkUnits).build();
+	public void testExecuteWorksResultSuccess() {
+		// Happy flow - all works completed
+		prepareWorks(works, null, -1);
+		executeWorks(works);
+		validateWorks(works, -1);
+	}
 
-		// when
-		sequentialFlow.execute(workContext);
+	@Test
+	public void testExecuteWorksResultFail() {
+		int failureWork = 0;
+		prepareWorks(works, failedWorkReport, failureWork);
+		executeWorks(works);
+		validateWorks(works, failureWork);
+	}
 
-		// then
-		InOrder inOrder = Mockito.inOrder(work1, work2, work3, work4);
-		inOrder.verify(work1, Mockito.times(1)).execute(workContext);
-		inOrder.verify(work2, Mockito.times(1)).execute(workContext);
-		inOrder.verify(work3, Mockito.times(1)).execute(workContext);
-		inOrder.verify(work4, Mockito.times(1)).execute(workContext);
+	@Test
+	public void testExecuteWorksResultInProgress() {
+		int failureWork = 2;
+		prepareWorks(works, progressWorkReport, failureWork);
+		executeWorks(works);
+		validateWorks(works, failureWork);
+	}
+
+	@Test
+	public void testExecuteWorksResultNull() {
+		int failureWork = 1;
+		prepareWorks(works, null, failureWork);
+		executeWorks(works);
+		validateWorks(works, failureWork);
+	}
+
+	@Test
+	public void testExecuteWorkUnitsResultSuccess() {
+		prepareWorks(works, null, -1);
+		executeWorkUnits(works);
+		validateWorks(works, -1);
+	}
+
+	@Test
+	public void testExecuteWorkUnitsResultFail() {
+		int failureWork = 0;
+		prepareWorks(works, failedWorkReport, failureWork);
+		executeWorkUnits(works);
+		validateWorks(works, failureWork);
+	}
+
+	@Test
+	public void testExecuteWorkUnitsResultInProgress() {
+		int failureWork = 2;
+		prepareWorks(works, progressWorkReport, failureWork);
+		executeWorkUnits(works);
+		validateWorks(works, failureWork);
+	}
+
+	@Test
+	public void testExecuteWorkUnitsResultNull() {
+		int failureWork = 1;
+		prepareWorks(works, null, failureWork);
+		executeWorkUnits(works);
+		validateWorks(works, failureWork);
+	}
+
+	private void executeWorks(Work[] works) {
+		SequentialFlow.Builder.aNewSequentialFlow().named("testFlow").execute(works[0]).then(works[1]).then(works[2])
+				.then(works[3]).build().execute(workContext);
+	}
+
+	private void executeWorkUnits(Work[] works) {
+		List<Work> initialWorkUnits = Arrays.asList(works[0], works[1]);
+		List<Work> nextWorkUnits = Arrays.asList(works[2], works[3]);
+
+		SequentialFlow.Builder.aNewSequentialFlow().named("testFlow").execute(initialWorkUnits).then(nextWorkUnits)
+				.build().execute(workContext);
+	}
+
+	private void prepareWorks(Work[] works, WorkReport errReport, int failureWork) {
+		for (int i = 0; i < works.length; i++) {
+			WorkReport report = completedWorkReport;
+			if (failureWork == i) {
+				report = errReport;
+			}
+			Mockito.when(works[i].execute(workContext)).thenReturn(report);
+			int count = i + 1;
+			Mockito.when(works[i].getName()).thenReturn("work#" + count);
+		}
+	}
+
+	private void validateWorks(Work[] works, int failureWork) {
+		InOrder inOrder = Mockito.inOrder(works);
+		for (int i = 0; i < works.length; i++) {
+			int runCount = 1;
+			if (failureWork >= 0 && failureWork < i) {
+				runCount = 0;
+			}
+			inOrder.verify(works[i], Mockito.times(runCount)).execute(workContext);
+		}
 	}
 
 }
