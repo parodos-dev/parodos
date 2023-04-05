c9f0d5629da684919f3ec7ac01df47654e49b741
Eloy Coto <eloy.coto@acalustra.com>
2023-04-05 10:28:43 +0200

    fix: cleanup workflow-properties issues
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

--- workflow-service/src/test/java/com/redhat/parodos/workflow/definition/dto/WorkFlowPropertiesDefinitionDTOTest.java
+++ workflow-service/src/test/java/com/redhat/parodos/workflow/definition/dto/WorkFlowPropertiesDefinitionDTOTest.java
@@ -3,7 +3,9 @@ package com.redhat.parodos.workflow.definition.dto;
 import com.redhat.parodos.workflow.definition.entity.WorkFlowPropertiesDefinition;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class WorkFlowPropertiesDefinitionDTOTest {
 
