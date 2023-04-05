61e7878c4d146ee032d9e74bc29aa66bca9ca908
Annel Ketcha <annelketcha@gmail.com>
2023-04-05 14:37:06 -0400

    add getters and setters

--- workflow-examples/src/main/java/com/redhat/parodos/examples/ocponboarding/task/dto/email/MessageRequestDTO.java
+++ workflow-examples/src/main/java/com/redhat/parodos/examples/ocponboarding/task/dto/email/MessageRequestDTO.java
@@ -32,4 +32,35 @@ public class MessageRequestDTO {
 		this.siteName = siteName;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getSiteName() {
+		return siteName;
+	}
+
+	public void setSiteName(String siteName) {
+		this.siteName = siteName;
+	}
 }
