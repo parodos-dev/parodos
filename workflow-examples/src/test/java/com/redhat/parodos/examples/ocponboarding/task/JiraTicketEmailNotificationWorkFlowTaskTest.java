937a17febd99cd3bb995f955eb7c0a1bdd93f2eb
Annel Ketcha <annelketcha@gmail.com>
2023-04-05 19:44:32 -0400

    fix test

--- workflow-examples/src/test/java/com/redhat/parodos/examples/ocponboarding/task/JiraTicketEmailNotificationWorkFlowTaskTest.java
+++ workflow-examples/src/test/java/com/redhat/parodos/examples/ocponboarding/task/JiraTicketEmailNotificationWorkFlowTaskTest.java
@@ -1,7 +1,12 @@
 package com.redhat.parodos.examples.ocponboarding.task;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
 import com.redhat.parodos.examples.base.BaseInfrastructureWorkFlowTaskTest;
-import com.redhat.parodos.examples.ocponboarding.task.dto.email.MessageRequestDTO;
 import com.redhat.parodos.examples.utils.RestUtils;
 import com.redhat.parodos.workflow.exception.MissingParameterException;
 import com.redhat.parodos.workflow.task.enums.WorkFlowTaskOutput;
@@ -10,20 +15,13 @@ import com.redhat.parodos.workflow.task.parameter.WorkFlowTaskParameter;
 import com.redhat.parodos.workflows.work.WorkContext;
 import com.redhat.parodos.workflows.work.WorkReport;
 import com.redhat.parodos.workflows.work.WorkStatus;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
-
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.spy;
 
 /**
  * Jira Ticket Email Notification Task execution test
@@ -36,7 +34,7 @@ public class JiraTicketEmailNotificationWorkFlowTaskTest extends BaseInfrastruct
 
 	private static final String mailServiceUrl = "mail-service-url-test";
 
-	private static final String jiraTicketUrl = "jira-ticket-url-test";
+	public static final String jiraTicketUrl = "jira-ticket-url-test";
 
 	private JiraTicketEmailNotificationWorkFlowTask jiraTicketEmailNotificationWorkFlowTask;
 
@@ -63,17 +61,15 @@ public class JiraTicketEmailNotificationWorkFlowTaskTest extends BaseInfrastruct
 	public void executeSuccess() {
 		// given
 		WorkContext workContext = Mockito.mock(WorkContext.class);
-		MessageRequestDTO messageRequestDTO = Mockito.mock(MessageRequestDTO.class);
-		HttpEntity<MessageRequestDTO> requestEntity = new HttpEntity<>(messageRequestDTO);
 		try (MockedStatic<RestUtils> restUtilsMockedStatic = Mockito.mockStatic(RestUtils.class)) {
-			restUtilsMockedStatic.when(() -> RestUtils.executePost(eq(mailServiceUrl), eq(requestEntity)))
+			restUtilsMockedStatic.when(() -> RestUtils.executePost(eq(mailServiceUrl), Mockito.any(HttpEntity.class)))
 					.thenReturn(ResponseEntity.ok("Mail Sent"));
 
 			// when
 			WorkReport workReport = jiraTicketEmailNotificationWorkFlowTask.execute(workContext);
 
 			// then
-			assertEquals(WorkStatus.FAILED, workReport.getStatus());
+			assertEquals(WorkStatus.COMPLETED, workReport.getStatus());
 		}
 	}
 
@@ -81,10 +77,8 @@ public class JiraTicketEmailNotificationWorkFlowTaskTest extends BaseInfrastruct
 	public void executeFail() {
 		// given
 		WorkContext workContext = Mockito.mock(WorkContext.class);
-		MessageRequestDTO messageRequestDTO = Mockito.mock(MessageRequestDTO.class);
-		HttpEntity<MessageRequestDTO> requestEntity = new HttpEntity<>(messageRequestDTO);
 		try (MockedStatic<RestUtils> restUtilsMockedStatic = Mockito.mockStatic(RestUtils.class)) {
-			restUtilsMockedStatic.when(() -> RestUtils.executePost(eq(mailServiceUrl), eq(requestEntity)))
+			restUtilsMockedStatic.when(() -> RestUtils.executePost(eq(mailServiceUrl), Mockito.any(HttpEntity.class)))
 					.thenReturn(ResponseEntity.internalServerError().build());
 
 			// when
