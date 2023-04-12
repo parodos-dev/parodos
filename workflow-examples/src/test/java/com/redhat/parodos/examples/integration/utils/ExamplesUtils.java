461452878d2514a534cec792b2153aa9b4dbf883
Gloria Ciavarrini <gciavarrini@redhat.com>
2023-04-12 11:19:22 +0200

    Reformat files
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>

--- workflow-examples/src/test/java/com/redhat/parodos/examples/integration/utils/ExamplesUtils.java
+++ workflow-examples/src/test/java/com/redhat/parodos/examples/integration/utils/ExamplesUtils.java
@@ -20,89 +20,96 @@ import static org.junit.Assert.fail;
  */
 @Slf4j
 public final class ExamplesUtils {
-    static final int maxTry = 100;
 
-    public static void waitProjectStart(ProjectApi projectApi) throws ApiException, InterruptedException {
+	static final int maxTry = 100;
 
-        AtomicInteger tryCount = new AtomicInteger(0);
-        AsyncResult<List<ProjectResponseDTO>> asyncResult = new AsyncResult<>();
-        ApiCallback<List<ProjectResponseDTO>> apiCallback = new ApiCallback<>() {
-            AtomicInteger failureCounter = new AtomicInteger(0);
+	public static void waitProjectStart(ProjectApi projectApi) throws ApiException, InterruptedException {
 
-            @Override
-            public void onFailure(ApiException e, int statusCode, Map<String, List<String>> responseHeaders) {
-                int i = failureCounter.incrementAndGet();
-                if (i >= 100) {
-                    asyncResult.setError(e.getMessage());
-                    asyncResult.setStatus(statusCode);
-                }
-            }
+		AtomicInteger tryCount = new AtomicInteger(0);
+		AsyncResult<List<ProjectResponseDTO>> asyncResult = new AsyncResult<>();
+		ApiCallback<List<ProjectResponseDTO>> apiCallback = new ApiCallback<>() {
+			AtomicInteger failureCounter = new AtomicInteger(0);
 
-            @Override
-            public void onSuccess(List<ProjectResponseDTO> result, int statusCode,
-                                  Map<String, List<String>> responseHeaders) {
-                asyncResult.setResponse(result);
-                asyncResult.setStatus(statusCode);
-            }
+			@Override
+			public void onFailure(ApiException e, int statusCode, Map<String, List<String>> responseHeaders) {
+				int i = failureCounter.incrementAndGet();
+				if (i >= 100) {
+					asyncResult.setError(e.getMessage());
+					asyncResult.setStatus(statusCode);
+				}
+			}
 
-            @Override
-            public void onUploadProgress(long bytesWritten, long contentLength, boolean done) {
-            }
+			@Override
+			public void onSuccess(List<ProjectResponseDTO> result, int statusCode,
+					Map<String, List<String>> responseHeaders) {
+				asyncResult.setResponse(result);
+				asyncResult.setStatus(statusCode);
+			}
 
-            @Override
-            public void onDownloadProgress(long bytesRead, long contentLength, boolean done) {
-            }
-        };
+			@Override
+			public void onUploadProgress(long bytesWritten, long contentLength, boolean done) {
+			}
 
+			@Override
+			public void onDownloadProgress(long bytesRead, long contentLength, boolean done) {
+			}
+		};
 
-        do {
-            if (tryCount.get() >= maxTry) {
-                fail("Can get results from getProjectsAsync, timeout reached.");
-            }
-            Thread.sleep(1000);
-            tryCount.incrementAndGet();
+		do {
+			if (tryCount.get() >= maxTry) {
+				fail("Can get results from getProjectsAsync, timeout reached.");
+			}
+			Thread.sleep(1000);
+			tryCount.incrementAndGet();
 
-            if (asyncResult.responseReceived()) {
-                if (asyncResult.getError() != null) {
-                    fail("An error occurred while executing getProjectsAsync: " + asyncResult.getError() + ". Status code: " + asyncResult.getStatus());
-                }
-                if (asyncResult.getResponse() != null) {
-                    break;
-                }
-                //retry async call
-                projectApi.getProjectsAsync(apiCallback);
-            }
-        } while (!asyncResult.responseReceived());
+			if (asyncResult.responseReceived()) {
+				if (asyncResult.getError() != null) {
+					fail("An error occurred while executing getProjectsAsync: " + asyncResult.getError()
+							+ ". Status code: " + asyncResult.getStatus());
+				}
+				if (asyncResult.getResponse() != null) {
+					break;
+				}
+				// retry async call
+				projectApi.getProjectsAsync(apiCallback);
+			}
+		}
+		while (!asyncResult.responseReceived());
 
-    }
+	}
 
-    @Nullable
-    public static ProjectResponseDTO getProjectByNameAndDescription(List<ProjectResponseDTO> projects,
-                                                                    String projectName, String projectDescription) {
-        return projects.stream()
-                .filter(prj -> projectName.equals(prj.getName()) && projectDescription.equals(prj.getDescription())
-                        && prj.getUsername() == null && !Strings.isNullOrEmpty(prj.getId()))
-                .findAny().orElse(null);
-    }
+	@Nullable
+	public static ProjectResponseDTO getProjectByNameAndDescription(List<ProjectResponseDTO> projects,
+			String projectName, String projectDescription) {
+		return projects.stream()
+				.filter(prj -> projectName.equals(prj.getName()) && projectDescription.equals(prj.getDescription())
+						&& prj.getUsername() == null && !Strings.isNullOrEmpty(prj.getId()))
+				.findAny().orElse(null);
+	}
 
-    @Data
-    private static class AsyncResult<T> {
-        private String error;
-        private Integer status;
-        private T response;
+	@Data
+	private static class AsyncResult<T> {
 
-        private AsyncResult() {
-            clear();
-        }
+		private String error;
 
-        private boolean responseReceived() {
-            return error != null && status != null && response != null;
-        }
+		private Integer status;
 
-        private void clear() {
-            error = null;
-            status = null;
-            response = null;
-        }
-    }
+		private T response;
+
+		private AsyncResult() {
+			clear();
+		}
+
+		private boolean responseReceived() {
+			return error != null && status != null && response != null;
+		}
+
+		private void clear() {
+			error = null;
+			status = null;
+			response = null;
+		}
+
+	}
+
 }
