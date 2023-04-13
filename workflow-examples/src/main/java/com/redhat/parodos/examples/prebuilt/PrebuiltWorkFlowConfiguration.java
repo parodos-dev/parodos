fb55a8ae36100ee8a62c615194c461b9f0061ebf
Nir Argaman <nargaman@redhat.com>
2023-04-13 10:21:28 +0300

    NotificationWorkFlowTask: Add new workflow Configuration uses the new Notification task
    Add new Workflow configuration for the prebuilt tasks
    At first include only Notification task, but add more pre-built tasks in the future

---
+++ workflow-examples/src/main/java/com/redhat/parodos/examples/prebuilt/PrebuiltWorkFlowConfiguration.java
@@ -0,0 +1,30 @@
+package com.redhat.parodos.examples.prebuilt;
+
+import com.redhat.parodos.tasks.notification.NotificationWorkFlowTask;
+import com.redhat.parodos.workflow.annotation.Infrastructure;
+import com.redhat.parodos.workflow.consts.WorkFlowConstants;
+import com.redhat.parodos.workflow.utils.CredUtils;
+import com.redhat.parodos.workflows.workflow.SequentialFlow;
+import com.redhat.parodos.workflows.workflow.WorkFlow;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PrebuiltWorkFlowConfiguration {
+
+	@Bean
+	NotificationWorkFlowTask notificationTask() {
+		return new NotificationWorkFlowTask("http://localhost:8081",
+				"Basic " + CredUtils.getBase64Creds("test", "test"));
+	}
+
+	@Bean(name = "prebuiltWorkFlow" + WorkFlowConstants.INFRASTRUCTURE_WORKFLOW)
+	@Infrastructure
+	WorkFlow prebuiltSequentialWorkFlow(@Qualifier("notificationTask") NotificationWorkFlowTask notificationTask) {
+		return SequentialFlow.Builder.aNewSequentialFlow()
+				.named("prebuiltWorkFlow" + WorkFlowConstants.INFRASTRUCTURE_WORKFLOW).execute(notificationTask)
+				.build();
+	}
+
+}
