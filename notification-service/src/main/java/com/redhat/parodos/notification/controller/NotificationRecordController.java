9a9f5baf4140bdd0a4ed16a2854e696e091b7ebf
Nir Argaman <nargaman@redhat.com>
2023-04-23 21:35:58 +0300

    Refactor NotificationRecordController - getNotifications function
    Change Get function to return "ResponseEntity<Page<NotificationRecordResponseDTO>>" instead of
    "PagedModel<NotificationRecordResponseDTO>" in order to fix miss matching between the returned value
    to the structure defined in swagger.
    In addition APIResponse annotations for the error codes

--- notification-service/src/main/java/com/redhat/parodos/notification/controller/NotificationRecordController.java
+++ notification-service/src/main/java/com/redhat/parodos/notification/controller/NotificationRecordController.java
@@ -17,24 +17,23 @@ package com.redhat.parodos.notification.controller;
 
 import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.data.web.PagedResourcesAssembler;
-import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-import com.redhat.parodos.notification.dto.NotificationAssemblerDTO;
 import com.redhat.parodos.notification.dto.NotificationRecordResponseDTO;
 import com.redhat.parodos.notification.enums.Operation;
 import com.redhat.parodos.notification.enums.State;
@@ -59,53 +58,67 @@ public class NotificationRecordController {
 
 	private final NotificationRecordService notificationRecordService;
 
-	private final PagedResourcesAssembler<NotificationRecord> notificationRecordPagedResourcesAssembler;
-
 	private final SecurityUtil securityUtil;
 
-	private final NotificationAssemblerDTO notificationAssemblerDTO = new NotificationAssemblerDTO();
-
 	public NotificationRecordController(NotificationRecordService notificationRecordService,
-			PagedResourcesAssembler<NotificationRecord> notificationRecordPagedResourcesAssembler,
 			SecurityUtil securityUtil) {
 		this.notificationRecordService = notificationRecordService;
-		this.notificationRecordPagedResourcesAssembler = notificationRecordPagedResourcesAssembler;
 		this.securityUtil = securityUtil;
 	}
 
 	/**
 	 * Returns all notifications for a user with option to apply filter or search term.
 	 */
+	@io.swagger.v3.oas.annotations.Operation(summary = "Return a list of notification records for the user")
+	@ApiResponses(
+			value = { @ApiResponse(responseCode = "200", description = "Successfully retrieved page of notifications"),
+					@ApiResponse(responseCode = "401", description = "Unauthorized"),
+					@ApiResponse(responseCode = "403", description = "Forbidden") })
 	@GetMapping
-	@ResponseStatus(HttpStatus.OK)
-	public PagedModel<NotificationRecordResponseDTO> getNotifications(@PageableDefault(size = 100) Pageable pageable,
+	public ResponseEntity<Page<NotificationRecordResponseDTO>> getNotifications(
+			@PageableDefault(size = 100) Pageable pageable,
 			@RequestParam(value = "state", required = false) State state,
 			@RequestParam(value = "searchTerm", required = false) String searchTerm) {
-		if (securityUtil == null || securityUtil.getUsername() == null) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No username associated with this request");
-		}
-		Page<NotificationRecord> notificationsRecordPage = this.notificationRecordService
-				.getNotificationRecords(pageable, securityUtil.getUsername(), state, searchTerm);
-		return this.notificationRecordPagedResourcesAssembler.toModel(notificationsRecordPage,
-				this.notificationAssemblerDTO);
+
+		Page<NotificationRecord> notificationsPage = this.notificationRecordService.getNotificationRecords(pageable,
+				securityUtil.getUsername(), state, searchTerm);
+		Page<NotificationRecordResponseDTO> notificationsDtoPage = notificationsPage
+				.map(notificationRecord -> NotificationRecordResponseDTO.toModel(notificationRecord));
+
+		return ResponseEntity.ok(notificationsDtoPage);
 	}
 
 	/**
-	 * Returns unread notifications for a user.
+	 * Returns the number of the unread notification records for the user.
 	 */
+	@io.swagger.v3.oas.annotations.Operation(
+			summary = "Return the number of the unread notification records for the user")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Successfully retrieved the amount of notifications"),
+			@ApiResponse(responseCode = "401", description = "Unauthorized"),
+			@ApiResponse(responseCode = "403", description = "Forbidden") })
 	@GetMapping("/count")
 	@ResponseStatus(HttpStatus.OK)
 	public int countUnreadNotifications(@RequestParam State state) {
 		return this.notificationRecordService.countNotificationRecords(securityUtil.getUsername(), state);
 	}
 
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Succeeded"),
+			@ApiResponse(responseCode = "401", description = "Unauthorized"),
+			@ApiResponse(responseCode = "404", description = "Not found") })
+	@io.swagger.v3.oas.annotations.Operation(summary = "Update the specified notification record with user operation")
 	@PutMapping("/{id}")
 	public NotificationRecordResponseDTO updateNotificationStatusById(@PathVariable("id") UUID id,
 			@RequestParam Operation operation) {
 		NotificationRecord notificationRecord = this.notificationRecordService.updateNotificationStatus(id, operation);
-		return notificationAssemblerDTO.toModel(notificationRecord);
+		return NotificationRecordResponseDTO.toModel(notificationRecord);
 	}
 
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Successfully retrieved the amount of notifications"),
+			@ApiResponse(responseCode = "401", description = "Unauthorized"),
+			@ApiResponse(responseCode = "403", description = "Forbidden") })
+	@io.swagger.v3.oas.annotations.Operation(summary = "Delete the specified notification record")
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteNotification(@PathVariable("id") UUID id) {
