039c1c464eef990233683c54e64142093215ef50
Roy Golan <rgolan@redhat.com>
2023-05-01 17:18:53 +0300

    Use the WorkFlowStatus instance instead of name()
    This should be as clear and as simple intead of comparing strings.
    
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- parodos-model-api/src/main/java/com/redhat/parodos/workflow/enums/WorkFlowStatus.java
+++ parodos-model-api/src/main/java/com/redhat/parodos/workflow/enums/WorkFlowStatus.java
@@ -26,12 +26,4 @@ public enum WorkFlowStatus {
 
 	FAILED, IN_PROGRESS, COMPLETED, PENDING, REJECTED;
 
-	public boolean isFailed() {
-		return FAILED.name().equals(name());
-	}
-
-	public boolean isRejected() {
-		return REJECTED.name().equals(name());
-	}
-
 }
