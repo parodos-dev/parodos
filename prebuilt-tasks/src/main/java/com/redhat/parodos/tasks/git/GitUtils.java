c7147dbd371d93b4235704b7ee88e129b85c60e7
Eloy Coto <eloy.coto@acalustra.com>
2023-05-02 17:40:21 +0200

    Feat: add git prebuilt-tasks
    This commit addressed a new way to clone and archive git repositories
    inside Parodos.
    
    The main objective for these tasks is to implement work on top of
    Move2kube.
    
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

---
+++ prebuilt-tasks/src/main/java/com/redhat/parodos/tasks/git/GitUtils.java
@@ -0,0 +1,41 @@
+package com.redhat.parodos.tasks.git;
+
+import com.redhat.parodos.workflow.context.WorkContextDelegate;
+import com.redhat.parodos.workflows.work.WorkContext;
+import lombok.Getter;
+
+public abstract class GitUtils {
+
+	private GitUtils() {
+	}
+
+	@Getter
+	static final String gitRepoPath = "path";
+
+	@Getter
+	static final String uri = "uri";
+
+	@Getter
+	static final String branch = "branch";
+
+	@Getter
+	static final String ContextUri = "gitUri";
+
+	@Getter
+	static final String ContextBranch = "gitBranch";
+
+	@Getter
+	static final String contextDestination = "gitDestination";
+
+	@Getter
+	static final String contextArchivePath = "gitArchivePath";
+
+	public static String getRepoPath(WorkContext workContext) {
+		var dest = workContext.get(contextDestination);
+		if (dest == null) {
+			return WorkContextDelegate.getOptionalValueFromRequestParams(workContext, getGitRepoPath(), "");
+		}
+		return WorkContextDelegate.getOptionalValueFromRequestParams(workContext, getGitRepoPath(), dest.toString());
+	}
+
+}
