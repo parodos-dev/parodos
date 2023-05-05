b0ef855b898363eb8950cd166416bb042af5dacb
Piotr Kliczewski <piotr.kliczewski@gmail.com>
2023-05-05 20:44:08 +0200

    Release 1.0.10

--- notification-service-sdk/src/main/java/com/redhat/parodos/notification/sdk/api/ApiClient.java
+++ notification-service-sdk/src/main/java/com/redhat/parodos/notification/sdk/api/ApiClient.java
@@ -147,7 +147,7 @@ public class ApiClient {
 		json = new JSON();
 
 		// Set default User-Agent.
-		setUserAgent("OpenAPI-Generator/1.0.10-SNAPSHOT/java");
+		setUserAgent("OpenAPI-Generator/1.0.10/java");
 
 		authentications = new HashMap<String, Authentication>();
 	}
