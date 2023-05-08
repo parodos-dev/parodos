1e323dea5cc45da361eee83d03dea0a41e46e8b5
Moti Asayag <masayag@redhat.com>
2023-05-08 15:44:13 +0300

    Add generated SDK code
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- workflow-service-sdk/docs/WorkFlowDefinitionResponseDTO.md
+++ workflow-service-sdk/docs/WorkFlowDefinitionResponseDTO.md
@@ -15,7 +15,7 @@
 |**parameters** | **Map&lt;String, Map&lt;String, Object&gt;&gt;** |  |  [optional] |
 |**processingType** | [**ProcessingTypeEnum**](#ProcessingTypeEnum) |  |  [optional] |
 |**properties** | [**WorkFlowPropertiesDefinitionDTO**](WorkFlowPropertiesDefinitionDTO.md) |  |  [optional] |
-|**type** | **String** |  |  [optional] |
+|**type** | [**TypeEnum**](#TypeEnum) |  |  [optional] |
 |**works** | [**List&lt;WorkDefinitionResponseDTO&gt;**](WorkDefinitionResponseDTO.md) |  |  [optional] |
 
 
@@ -27,6 +27,17 @@
 | SEQUENTIAL | &quot;SEQUENTIAL&quot; |
 | PARALLEL | &quot;PARALLEL&quot; |
 | OTHER | &quot;OTHER&quot; |
+
+
+
+## Enum: TypeEnum
+
+| Name | Value |
+|---- | -----|
+| ASSESSMENT | &quot;ASSESSMENT&quot; |
+| CHECKER | &quot;CHECKER&quot; |
+| INFRASTRUCTURE | &quot;INFRASTRUCTURE&quot; |
+| ESCALATION | &quot;ESCALATION&quot; |
 
 
 
--- workflow-service-sdk/src/main/java/com/redhat/parodos/sdk/model/WorkFlowDefinitionResponseDTO.java
+++ workflow-service-sdk/src/main/java/com/redhat/parodos/sdk/model/WorkFlowDefinitionResponseDTO.java
@@ -136,10 +136,65 @@ public class WorkFlowDefinitionResponseDTO {
 	@SerializedName(SERIALIZED_NAME_PROPERTIES)
 	private WorkFlowPropertiesDefinitionDTO properties;
 
+	/**
+	 * Gets or Sets type
+	 */
+	@JsonAdapter(TypeEnum.Adapter.class)
+	public enum TypeEnum {
+
+		ASSESSMENT("ASSESSMENT"),
+
+		CHECKER("CHECKER"),
+
+		INFRASTRUCTURE("INFRASTRUCTURE"),
+
+		ESCALATION("ESCALATION");
+
+		private String value;
+
+		TypeEnum(String value) {
+			this.value = value;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		@Override
+		public String toString() {
+			return String.valueOf(value);
+		}
+
+		public static TypeEnum fromValue(String value) {
+			for (TypeEnum b : TypeEnum.values()) {
+				if (b.value.equals(value)) {
+					return b;
+				}
+			}
+			throw new IllegalArgumentException("Unexpected value '" + value + "'");
+		}
+
+		public static class Adapter extends TypeAdapter<TypeEnum> {
+
+			@Override
+			public void write(final JsonWriter jsonWriter, final TypeEnum enumeration) throws IOException {
+				jsonWriter.value(enumeration.getValue());
+			}
+
+			@Override
+			public TypeEnum read(final JsonReader jsonReader) throws IOException {
+				String value = jsonReader.nextString();
+				return TypeEnum.fromValue(value);
+			}
+
+		}
+
+	}
+
 	public static final String SERIALIZED_NAME_TYPE = "type";
 
 	@SerializedName(SERIALIZED_NAME_TYPE)
-	private String type;
+	private TypeEnum type;
 
 	public static final String SERIALIZED_NAME_WORKS = "works";
 
@@ -317,7 +372,7 @@ public class WorkFlowDefinitionResponseDTO {
 		this.properties = properties;
 	}
 
-	public WorkFlowDefinitionResponseDTO type(String type) {
+	public WorkFlowDefinitionResponseDTO type(TypeEnum type) {
 
 		this.type = type;
 		return this;
@@ -329,11 +384,11 @@ public class WorkFlowDefinitionResponseDTO {
 	 **/
 	@javax.annotation.Nullable
 
-	public String getType() {
+	public TypeEnum getType() {
 		return type;
 	}
 
-	public void setType(String type) {
+	public void setType(TypeEnum type) {
 		this.type = type;
 	}
 
