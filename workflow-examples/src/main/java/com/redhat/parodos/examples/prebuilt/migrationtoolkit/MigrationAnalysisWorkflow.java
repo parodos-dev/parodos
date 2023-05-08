ad169d35192370ced3ea2268707b6a736009d4ac
Roy Golan <rgolan@redhat.com>
2023-05-08 17:06:12 +0300

    examples: An Analysis application workflow
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- workflow-examples/src/main/java/com/redhat/parodos/examples/prebuilt/migrationtoolkit/MigrationAnalysisWorkflow.java
+++ workflow-examples/src/main/java/com/redhat/parodos/examples/prebuilt/migrationtoolkit/MigrationAnalysisWorkflow.java
@@ -1,117 +1,122 @@
 package com.redhat.parodos.examples.prebuilt.migrationtoolkit;
 
-import com.redhat.parodos.examples.escalation.checker.SimpleTaskOneChecker;
-import com.redhat.parodos.examples.escalation.task.SimpleTaskOne;
-import com.redhat.parodos.examples.escalation.task.SimpleTaskOneEscalator;
-import com.redhat.parodos.examples.escalation.task.SimpleTaskTwo;
-import com.redhat.parodos.tasks.migrationtoolkit.CreateApplicationTask;
-import com.redhat.parodos.tasks.migrationtoolkit.GetAnalysisTask;
-import com.redhat.parodos.tasks.migrationtoolkit.GetApplicationTask;
-import com.redhat.parodos.tasks.migrationtoolkit.SubmitAnalysisTask;
+import java.net.URI;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+import com.redhat.parodos.email.Message;
+import com.redhat.parodos.tasks.migrationtoolkit.*;
 import com.redhat.parodos.workflow.annotation.Checker;
-import com.redhat.parodos.workflow.annotation.Escalation;
 import com.redhat.parodos.workflow.annotation.Infrastructure;
-import com.redhat.parodos.workflows.workflow.SequentialFlow;
 import com.redhat.parodos.workflows.workflow.WorkFlow;
-import org.jose4j.http.Get;
-import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-import java.util.Arrays;
-import java.util.Date;
+import static com.redhat.parodos.workflows.workflow.SequentialFlow.Builder.aNewSequentialFlow;
 
+/**
+ * A workflow to analyze applications using Migration Toolkit for Applications
+ *
+ * This workflow will: - create an application with a name, and git repo URL - submit an
+ * analysis report for it - wait till the report is back - send an email with the report
+ * url
+ *
+ * ENV Variables: For security reasons the MTA url and bearer token can be passed to all
+ * the task constructors For example use MTA_URL and MTA_BEARER_TOKEN To get an email
+ * supply the following variables MAILER_HOST MAILER_PORT MAILER_USER MAILER_PASS
+ */
+@Profile("examples")
 @Configuration
 public class MigrationAnalysisWorkflow {
 
-    @Bean
-    @Infrastructure
-    public WorkFlow AnalyzeApplication (
-            CreateApplicationTask createAppTask,
-            GetApplicationTask getAppTask,
-            SubmitAnalysisTask submitAnalysisTask,
-            GetAnalysisTask getAnalysisTask) {
-            return SequentialFlow.Builder
-                    .aNewSequentialFlow()
-                    .named("migration_toolkit_analyze_application")
-                    .execute(createAppTask)
-                    .then(getAppTask)
-                    .then(submitAnalysisTask)
-                    .build();
-            // @formatter:on
-    }
+	// the url of the MTA (migration toolkit for application)
+	private String mtaUrl;
 
-    // Task executed by workflowStartingCheckingAndEscalation
-    @Bean
-    public GetAnalysisTask getAnalysisTask (
-            @Qualifier("getAnalysisTaskChecker") WorkFlow simpleTaskOneCheckerWorkflow) {
-        GetAnalysisTask taskOne = new SimpleTaskOne();
-        taskOne.setWorkFlowCheckers(Arrays.asList(simpleTaskOneCheckerWorkflow));
-        return taskOne;
-    }
+	// in the end you can send a message to an email server with the fields below. The
+	// email code
+	// can be removed from the workflow configuration if not needed or be replaced with
+	// any messaging system.
+	// See #messageConsumer
+	// If you do remove or change the messaging consumer then make sure to update the
+	// constructor checks.
+	private final String mailerHost;
 
+	private final String mailerPort;
 
-    // ********** Start Checker + Escalation **************
-    @Bean
-    @Checker(cronExpression = "*/5 * * * * ?")
-    public WorkFlow simpleTaskOneCheckerWorkflow(
-            @Qualifier("simpleTaskOneCheckerTask") SimpleTaskOneChecker simpleTaskOneCheckerTask) {
-        // @formatter:off
-            return SequentialFlow.Builder
-                    .aNewSequentialFlow()
-                    .named("simpleTaskOneCheckerWorkflow")
-                    .execute(simpleTaskOneCheckerTask)
-                    .build();
-            // @formatter:on
-    }
+	private final String mailerUser;
 
-    @Bean
-    public SimpleTaskOneChecker simpleTaskOneCheckerTask(
-            @Qualifier("simpleTaskOneEscalatorWorkflow") WorkFlow simpleTaskOneEscalatorWorkflow) {
-        return new SimpleTaskOneChecker(simpleTaskOneEscalatorWorkflow, new Date().getTime() / 1000 + 30);
-    }
+	private final String mailerPass;
 
-    @Bean
-    @Escalation
-    public WorkFlow simpleTaskOneEscalatorWorkflow(
-            @Qualifier("simpleTaskOneEscalator") SimpleTaskOneEscalator simpleTaskOneEscalator) {
-        // @formatter:off
-            return SequentialFlow.Builder
-                    .aNewSequentialFlow()
-                    .named("simpleTaskOneEscalatorWorkflow")
-                    .execute(simpleTaskOneEscalator)
-                    .build();
-            // @formatter:on
+	public MigrationAnalysisWorkflow() {
+		mtaUrl = Objects.requireNonNull(System.getenv("MTA_URL"));
+		mailerHost = Objects.requireNonNull(System.getenv("MAILER_HOST"));
+		mailerPort = Objects.requireNonNull(System.getenv("MAILER_PORT"));
+		mailerUser = Objects.requireNonNull(System.getenv("MAILER_USER"));
+		mailerPass = Objects.requireNonNull(System.getenv("MAILER_PASS"));
+	}
 
-    }
+	@Bean
+	public CreateApplicationTask createApplicationTask() {
+		return new CreateApplicationTask(URI.create(mtaUrl), "");
+	}
 
-    @Bean
-    public SimpleTaskOneEscalator simpleTaskOneEscalator() {
-        return new SimpleTaskOneEscalator();
-    }
+	@Bean
+	public GetApplicationTask getApplicationTask() {
+		return new GetApplicationTask(URI.create(mtaUrl), "");
+	}
 
-    // ********** End Checker + Escalation **************
+	@Bean
+	public SubmitAnalysisTask submitAnalysisTask(WorkFlow fetchReportURL) {
+		SubmitAnalysisTask t = new SubmitAnalysisTask(URI.create(mtaUrl), "");
+		t.setWorkFlowCheckers(List.of(fetchReportURL));
+		return t;
+	}
 
-    // ********** Start Second Workflow **************
-    // This is the workflow that runs if simpleTaskOneCheckerWorkflow (Checker for task
-    // SimpleTaskOne) is successful
-    @Bean
-    @Infrastructure
-    public WorkFlow workflowContinuesAfterCheckingEscalation(SimpleTaskTwo simpleTaskTwo) {
-        // @formatter:off
-            return SequentialFlow.Builder
-                    .aNewSequentialFlow()
-                    .named("workflowContinuesAfterCheckingEscalation")
-                    .execute(simpleTaskTwo)
-                    .build();
-            // @formatter:on
-    }
+	@Bean
+	public GetAnalysisTask getAnalysisTask() {
+		return new GetAnalysisTask(URI.create(mtaUrl), "", messageConsumer());
+	}
 
-    @Bean
-    public SimpleTaskTwo simpleTaskTwo() {
-        return new SimpleTaskTwo();
-    }
-    // ********** End Second Workflow **************
+	@Bean(name = "AnalyzeApplication")
+	@Infrastructure
+	public WorkFlow AnalyzeApplication(CreateApplicationTask createApplicationTask, GetApplicationTask getAppTask,
+			SubmitAnalysisTask submitAnalysisTask) {
+		return aNewSequentialFlow().named("AnalyzeApplication").execute(createApplicationTask).then(getAppTask)
+				.then(submitAnalysisTask).build();
+	}
 
-}
+	@Bean("fetchReportURL")
+	@Checker(cronExpression = "*/5 * * * * ?")
+	public WorkFlow fetchReportURL(GetAnalysisTask getAnalysisTask) {
+		return aNewSequentialFlow().named("fetchReportURL").execute(getAnalysisTask).build();
+	}
+
+	private Consumer<Message> messageConsumer() {
+		var sender = new JavaMailSenderImpl();
+		sender.setHost(mailerHost);
+		sender.setPort(Integer.valueOf(mailerPort));
+		sender.setUsername(mailerUser);
+		sender.setPassword(mailerPass);
+
+		Consumer<Message> messageConsumer = m -> {
+			var message = new SimpleMailMessage();
+			message.setTo(m.to());
+			message.setFrom(m.from());
+			message.setSubject(m.subject());
+			message.setText(m.data());
+			try {
+				sender.send(message);
+			}
+			catch (Exception e) {
+				// TODO log handling
+			}
+		};
+		return messageConsumer;
+	}
+
 }
