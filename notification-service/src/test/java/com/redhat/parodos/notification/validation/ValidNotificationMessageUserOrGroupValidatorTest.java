2c9c3434f673549be0a7657df198a87e7feea768
Nir Argaman <nargaman@redhat.com>
2023-05-09 09:13:09 +0300

    notification-service: add User or Group validation to NotificationMessage creation
    When NotificationMessage is created, user must be assigned to it, this can be done by indicating
    GroupName (all users under this group are assigned to the Notification)
    Therefore, create API must include validation that there are users or groups added to the request

---
+++ notification-service/src/test/java/com/redhat/parodos/notification/validation/ValidNotificationMessageUserOrGroupValidatorTest.java
@@ -0,0 +1,63 @@
+package com.redhat.parodos.notification.validation;
+
+import java.util.List;
+
+import javax.validation.ConstraintValidatorContext;
+
+import com.redhat.parodos.notification.dto.NotificationMessageCreateRequestDTO;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class ValidNotificationMessageUserOrGroupValidatorTest {
+
+	private ValidNotificationMessageUserOrGroupValidator validator;
+
+	private ConstraintValidatorContext constraintValidatorContext;
+
+	@Before
+	public void setUp() {
+		validator = new ValidNotificationMessageUserOrGroupValidator();
+		constraintValidatorContext = mock(ConstraintValidatorContext.class);
+	}
+
+	@Test
+	public void shouldReturnTrueWhenValidUsernames() {
+		NotificationMessageCreateRequestDTO notificationMessage = new NotificationMessageCreateRequestDTO();
+		notificationMessage.setUsernames(List.of("test"));
+
+		assertThat(validator.isValid(notificationMessage, constraintValidatorContext)).isTrue();
+	}
+
+	@Test
+	public void shouldReturnTrueWhenValidGroupNames() {
+		NotificationMessageCreateRequestDTO notificationMessage = new NotificationMessageCreateRequestDTO();
+		notificationMessage.setGroupNames(List.of("test"));
+
+		assertThat(validator.isValid(notificationMessage, constraintValidatorContext)).isTrue();
+	}
+
+	@Test
+	public void shouldReturnTrueWhenBothValid() {
+		NotificationMessageCreateRequestDTO notificationMessage = new NotificationMessageCreateRequestDTO();
+		notificationMessage.setUsernames(List.of("test"));
+		notificationMessage.setGroupNames(List.of("test"));
+
+		assertThat(validator.isValid(notificationMessage, constraintValidatorContext)).isTrue();
+	}
+
+	@Test
+	public void shouldReturnFalseWhenBothEmpty() {
+		NotificationMessageCreateRequestDTO notificationMessage = new NotificationMessageCreateRequestDTO();
+
+		assertThat(validator.isValid(notificationMessage, constraintValidatorContext)).isFalse();
+	}
+
+	@Test
+	public void shouldReturnFalseWhenNotificationMessageEmpty() {
+		assertThat(validator.isValid(null, constraintValidatorContext)).isFalse();
+	}
+
+}
