e8ff0816f783101401b500dc11e6b7f69c835bd1
Piotr Kliczewski <piotr.kliczewski@gmail.com>
2023-05-10 12:51:43 +0200

    Fix tibco link
    We can obtain new link from: https://www.tibco.com/resources/product-download/tibco-enterprise-message-service-community-edition-free-download-linux
    
    Signed-off-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>

--- external-dependencies/download-tibco.sh
+++ external-dependencies/download-tibco.sh
@@ -2,7 +2,7 @@
 if [ -f "tibjms.jar" ]; then exit 0; fi
 rm -rf /tmp/tibco
 mkdir /tmp/tibco
-curl -s -o /tmp/tibco/tibco.zip "https://edownloads.tibco.com/Installers/tap/EMS-CE/10.2.1/TIB_ems-ce_10.2.1_linux_x86_64.zip?SJCDPTPG=1683881528_ae3eee42628d3f74b67cb52ea15aea45&ext=.zip"
+curl -s -o /tmp/tibco/tibco.zip "https://edownloads.tibco.com/Installers/tap/EMS-CE/10.2.1/TIB_ems-ce_10.2.1_linux_x86_64.zip?SJCDPTPG=1686307326_ef68d106a432fba5bdbcb60665c8b2b1&ext=.zip"
 cd /tmp/tibco
 unzip tibco.zip TIB_ems-ce_10.2.1/tar/TIB_ems-ce_10.2.1_linux_x86_64-java_client.tar.gz
 tar -zxf TIB_ems-ce_10.2.1/tar/TIB_ems-ce_10.2.1_linux_x86_64-java_client.tar.gz  opt/tibco/ems/10.2/lib/tibjms.jar
