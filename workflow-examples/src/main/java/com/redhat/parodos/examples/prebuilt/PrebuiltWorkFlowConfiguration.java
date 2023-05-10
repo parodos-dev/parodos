ae6e9b9373f9a19fd10ee78e9e72460d531ac944
Gloria Ciavarrini <gciavarrini@redhat.com>
2023-05-10 15:02:27 +0200

    Use env var in PrebuiltWorkFlowConfiguration
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>

--- workflow-examples/src/main/java/com/redhat/parodos/examples/prebuilt/PrebuiltWorkFlowConfiguration.java
+++ workflow-examples/src/main/java/com/redhat/parodos/examples/prebuilt/PrebuiltWorkFlowConfiguration.java
@@ -1,22 +1,30 @@
 package com.redhat.parodos.examples.prebuilt;
 
+import java.util.Optional;
+
 import com.redhat.parodos.tasks.notification.NotificationWorkFlowTask;
 import com.redhat.parodos.workflow.annotation.Infrastructure;
 import com.redhat.parodos.workflow.consts.WorkFlowConstants;
 import com.redhat.parodos.workflow.utils.CredUtils;
 import com.redhat.parodos.workflows.workflow.SequentialFlow;
 import com.redhat.parodos.workflows.workflow.WorkFlow;
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class PrebuiltWorkFlowConfiguration {
 
 	@Bean
 	NotificationWorkFlowTask notificationTask() {
-		return new NotificationWorkFlowTask("http://localhost:8081",
+		String serverIp = Optional.ofNullable(System.getenv("NOTIFICATION_SERVER_ADDRESS")).orElse("localhost");
+		String serverPort = Optional.ofNullable(System.getenv("NOTIFICATION_SERVER_PORT")).orElse("8080");
+		String notificationWorkFlowBasePath = "http://" + serverIp + ":" + serverPort;
+		log.info("NotificationWorkFlowTask basePath: {}", notificationWorkFlowBasePath);
+		return new NotificationWorkFlowTask(notificationWorkFlowBasePath,
 				"Basic " + CredUtils.getBase64Creds("test", "test"));
 	}
 
