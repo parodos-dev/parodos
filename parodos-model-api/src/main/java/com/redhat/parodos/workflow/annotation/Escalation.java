7506054bed68b4c8070549b22cfda610929a0200
Moti Asayag <masayag@redhat.com>
2023-05-17 20:58:28 +0300

    Enable checkstyle for parodos-model-api
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- parodos-model-api/src/main/java/com/redhat/parodos/workflow/annotation/Escalation.java
+++ parodos-model-api/src/main/java/com/redhat/parodos/workflow/annotation/Escalation.java
@@ -26,7 +26,7 @@ import java.lang.annotation.Target;
  *
  * @author Luke Shannon (Github: lshannon)
  * @author Gloria Ciavarrini (Github: gciavarrini)
- * 
+ *
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
