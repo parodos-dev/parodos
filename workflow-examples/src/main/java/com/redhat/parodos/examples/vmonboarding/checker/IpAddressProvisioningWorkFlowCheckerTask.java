f5430960a3f688acefdd28a0f2cb3af16dea81ce
richardwang98 <richwong98@gmail.com>
2023-05-18 09:16:54 -0400

    vm demo api

--- workflow-examples/src/main/java/com/redhat/parodos/examples/vmonboarding/checker/IpAddressProvisioningWorkFlowCheckerTask.java
+++ workflow-examples/src/main/java/com/redhat/parodos/examples/vmonboarding/checker/IpAddressProvisioningWorkFlowCheckerTask.java
@@ -15,6 +15,9 @@
  */
 package com.redhat.parodos.examples.vmonboarding.checker;
 
+import com.redhat.parodos.examples.utils.RestUtils;
+import com.redhat.parodos.examples.vmonboarding.dto.AapGetJobResponseDto;
+import com.redhat.parodos.workflow.exception.MissingParameterException;
 import com.redhat.parodos.workflow.task.checker.BaseWorkFlowCheckerTask;
 import com.redhat.parodos.workflows.work.DefaultWorkReport;
 import com.redhat.parodos.workflows.work.WorkContext;
@@ -22,6 +25,9 @@ import com.redhat.parodos.workflows.work.WorkReport;
 import com.redhat.parodos.workflows.work.WorkStatus;
 import com.redhat.parodos.workflows.workflow.WorkFlow;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
 
 /**
  * An example of a task that checks for IP address in a ticket
@@ -32,9 +38,18 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class IpAddressProvisioningWorkFlowCheckerTask extends BaseWorkFlowCheckerTask {
 
+	private final String aapUrl;
+
+	private final String username;
+
+	private final String password;
+
 	public IpAddressProvisioningWorkFlowCheckerTask(WorkFlow serviceNowTicketFulfillmentEscalationWorkFlowTask,
-			long sla) {
+			long sla, String aapUrl, String username, String password) {
 		super(serviceNowTicketFulfillmentEscalationWorkFlowTask, sla);
+		this.aapUrl = aapUrl;
+		this.username = username;
+		this.password = password;
 	}
 
 	/**
@@ -43,6 +58,34 @@ public class IpAddressProvisioningWorkFlowCheckerTask extends BaseWorkFlowChecke
 	@Override
 	public WorkReport checkWorkFlowStatus(WorkContext workContext) {
 		log.info("Start IpAddressProvisioningWorkFlowCheckerTask...");
+		try {
+			String jobId = getRequiredParameterValue(workContext, "JOB_ID");
+			log.info("job id: {}", jobId);
+			String urlString = aapUrl + "/api/v2/jobs/" + jobId;
+
+			ResponseEntity<AapGetJobResponseDto> result = RestUtils.restExchange(urlString, username, password,
+					AapGetJobResponseDto.class);
+			AapGetJobResponseDto responseDto = result.getBody();
+
+			if (!result.getStatusCode().is2xxSuccessful() || responseDto == null) {
+				log.error("Call to the API was not successful. Response: {}", result.getStatusCode());
+			}
+			else if (responseDto.getArtifacts().getAzureVmPublicIp() == null) {
+				log.error("ip is not ready...");
+			}
+			else {
+				String ip = responseDto.getArtifacts().getAzureVmPublicIp();
+				log.info("vm IP is: {}", ip);
+				addParameter(workContext, "IP", ip);
+				return new DefaultWorkReport(WorkStatus.COMPLETED, workContext);
+			}
+		}
+		catch (RestClientException e) {
+			log.error("There was an issue with the REST call: {}", e.getMessage());
+		}
+		catch (MissingParameterException e) {
+			log.error("There was an error getting parameter(s): {}", e.getMessage());
+		}
 		return new DefaultWorkReport(WorkStatus.FAILED, workContext);
 	}
 
--- workflow-examples/src/main/java/com/redhat/parodos/examples/vmonboarding/task/ServiceNowTicketCreationWorkFlowTask.java
+++ workflow-examples/src/main/java/com/redhat/parodos/examples/vmonboarding/task/ServiceNowTicketCreationWorkFlowTask.java
@@ -1,11 +1,18 @@
 package com.redhat.parodos.examples.vmonboarding.task;
 
+import com.redhat.parodos.examples.utils.RestUtils;
+import com.redhat.parodos.examples.vmonboarding.dto.GetRequestItemResponseDto;
+import com.redhat.parodos.examples.vmonboarding.dto.OrderServiceCatalogItemRequestDto;
+import com.redhat.parodos.examples.vmonboarding.dto.OrderServiceCatalogItemRequestVariable;
+import com.redhat.parodos.examples.vmonboarding.dto.OrderServiceCatalogItemResponseDto;
 import com.redhat.parodos.workflow.task.infrastructure.BaseInfrastructureWorkFlowTask;
 import com.redhat.parodos.workflows.work.DefaultWorkReport;
 import com.redhat.parodos.workflows.work.WorkContext;
 import com.redhat.parodos.workflows.work.WorkReport;
 import com.redhat.parodos.workflows.work.WorkStatus;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.ResponseEntity;
 
 /**
  * An example of a task that create a serviceNow ticket
@@ -16,10 +23,68 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ServiceNowTicketCreationWorkFlowTask extends BaseInfrastructureWorkFlowTask {
 
+	private final String serviceNowUrl;
+
+	private final String username;
+
+	private final String password;
+
+	private final String rhelSysId;
+
+	private static final String SERVICE_CATALOG_CONTEXT_PATH = "/api/sn_sc/servicecatalog/items/%s/order_now";
+
+	private static final String GET_ITEMS_BY_REQUEST_CONTEXT_PATH = "/api/now/table/sc_req_item?sysparm_query=%s&sysparm_limit=1";
+
+	public ServiceNowTicketCreationWorkFlowTask(String serviceNowUrl, String rhelSysId, String username,
+			String password) {
+		this.serviceNowUrl = serviceNowUrl;
+		this.rhelSysId = rhelSysId;
+		this.username = username;
+		this.password = password;
+	}
+
 	@Override
 	public WorkReport execute(WorkContext workContext) {
 		log.info("Start ServiceNowTicketCreationWorkFlowTask...");
-		return new DefaultWorkReport(WorkStatus.FAILED, workContext);
+		try {
+			String urlString = serviceNowUrl + String.format(SERVICE_CATALOG_CONTEXT_PATH, rhelSysId);
+			String vmName = getOptionalParameterValue(workContext, "hostname", "snowrhel");
+
+			log.info("vm name: {}", vmName);
+
+			OrderServiceCatalogItemRequestDto request = OrderServiceCatalogItemRequestDto.builder().quantity("1")
+					.variables(OrderServiceCatalogItemRequestVariable.builder().vmName(vmName).build()).build();
+
+			ResponseEntity<OrderServiceCatalogItemResponseDto> response = RestUtils.executePost(urlString, request,
+					username, password, OrderServiceCatalogItemResponseDto.class);
+
+			if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
+				String sysId = response.getBody().getResult().getSysId();
+				String requestNumber = response.getBody().getResult().getNumber();
+				log.info("Rest call completed, sys id: {}, request number: {}", sysId, requestNumber);
+				Thread.sleep(7000);
+				String getItemsByRequestUrl = serviceNowUrl
+						+ String.format(GET_ITEMS_BY_REQUEST_CONTEXT_PATH, "request.number=" + requestNumber);
+				ResponseEntity<GetRequestItemResponseDto> getItemsResponse = RestUtils
+						.restExchange(getItemsByRequestUrl, username, password, GetRequestItemResponseDto.class);
+
+				if (getItemsResponse.getStatusCode().is2xxSuccessful() && getItemsResponse.getBody() != null) {
+					log.info("Rest call completed, request item number: {}",
+							getItemsResponse.getBody().getResult().get(0).getNumber());
+					addParameter(workContext, "JOB_ID",
+							getItemsResponse.getBody().getResult().get(0).getShortDescription());
+					return new DefaultWorkReport(WorkStatus.COMPLETED, workContext);
+				}
+				log.error("Call to the API was not successful. Response: {}", getItemsResponse.getStatusCode());
+			}
+			log.error("Call to the API was not successful. Response: {}", response.getStatusCode());
+		}
+		catch (Exception e) {
+			log.error("There was an issue with the REST call: {}", e.getMessage());
+		}
+		return new
+
+		DefaultWorkReport(WorkStatus.FAILED, workContext);
 	}
 
 }
