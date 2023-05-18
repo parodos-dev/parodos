d159dfdd53c87db3163adbc395aafc000d0cbe0f
richardwang98 <richwong98@gmail.com>
2023-05-18 15:01:42 -0400

    vm demo api

--- workflow-examples/src/main/java/com/redhat/parodos/examples/vmonboarding/dto/AapGetJobResponseDTO.java
+++ workflow-examples/src/main/java/com/redhat/parodos/examples/vmonboarding/dto/AapGetJobResponseDTO.java
@@ -15,6 +15,9 @@ public class AapGetJobResponseDTO {
 	@JsonProperty("job")
 	private String jobId;
 
+	@JsonProperty("job")
+	private String jobId;
+
 	private String status;
 
 	private AapGetJobResponseArtifacts artifacts;
