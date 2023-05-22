9b8f72659fed0afa2cb484bf106ed301eb348483
Moti Asayag <masayag@redhat.com>
2023-05-22 16:39:57 +0300

    Extract shared code in integration-tests
    Each integration-tests that execute a workflow share the same structure:
    1. Create a project
    2. Verify workflow-definition exists
    3. Run a workflow
    
    The projects are created the same and can be placed in a shared location
    in which the project creation is part of test initialization.
    
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- integration-tests/src/test/java/com/redhat/parodos/flows/EscalationFlowTest.java
+++ integration-tests/src/test/java/com/redhat/parodos/flows/EscalationFlowTest.java
@@ -3,7 +3,6 @@ package com.redhat.parodos.flows;
 import com.redhat.parodos.flows.base.BaseIntegrationTest;
 import com.redhat.parodos.sdk.api.WorkflowApi;
 import com.redhat.parodos.sdk.invoker.ApiException;
-import com.redhat.parodos.sdk.model.ProjectResponseDTO;
 import com.redhat.parodos.sdk.model.WorkFlowRequestDTO;
 import com.redhat.parodos.sdk.model.WorkFlowResponseDTO;
 import com.redhat.parodos.sdk.model.WorkFlowStatusResponseDTO;
@@ -11,7 +10,6 @@ import com.redhat.parodos.sdkutils.SdkUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
-import static com.redhat.parodos.sdkutils.SdkUtils.getProjectAsync;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -21,14 +19,9 @@ import static org.junit.Assert.assertNotNull;
 @Slf4j
 public class EscalationFlowTest extends BaseIntegrationTest {
 
-	private static final String projectName = "project-1";
-
-	private static final String projectDescription = "an example project";
-
 	@Test
 	public void runEscalationFlow() throws ApiException, InterruptedException {
 		log.info("Running escalation flow");
-		ProjectResponseDTO testProject = getProjectAsync(apiClient, projectName, projectDescription);
 		WorkflowApi workflowApi = new WorkflowApi();
 
 		log.info("******** Running The Escalation WorkFlow ********");
