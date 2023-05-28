66d7f63ae3c7de09970c618d53445f0a9f6bdded
Yaron Dayagi <ydayagi@redhat.com>
2023-05-28 09:27:39 +0300

    Add a workflow task that uses JDBC
    FLPATH-215 https://issues.redhat.com/browse/FLPATH-215
    
    Signed-off-by: Yaron Dayagi <ydayagi@redhat.com>

---
+++ workflow-examples/src/main/java/com/redhat/parodos/examples/JdbcWorkFlowConfiguration.java
@@ -0,0 +1,27 @@
+package com.redhat.parodos.examples;
+
+import com.redhat.parodos.tasks.jdbc.JdbcWorkFlowTask;
+import com.redhat.parodos.workflow.annotation.Infrastructure;
+import com.redhat.parodos.workflows.workflow.SequentialFlow;
+import com.redhat.parodos.workflows.workflow.WorkFlow;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("jdbc")
+public class JdbcWorkFlowConfiguration {
+
+	@Bean
+	JdbcWorkFlowTask jdbcTask() {
+		return new JdbcWorkFlowTask();
+	}
+
+	@Bean
+	@Infrastructure
+	WorkFlow jdbcWorkFlow(JdbcWorkFlowTask jdbcTask) {
+		return SequentialFlow.Builder.aNewSequentialFlow().named("jdbcWorkFlow").execute(jdbcTask).build();
+	}
+
+}
