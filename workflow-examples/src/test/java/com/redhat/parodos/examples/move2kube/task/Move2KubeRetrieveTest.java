1acd172c8564ea1eeae4746faff71ad9f3386f18
Moti Asayag <masayag@redhat.com>
2023-05-30 10:55:07 +0300

    Replace powermock.mock with mockito.mock
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- workflow-examples/src/test/java/com/redhat/parodos/examples/move2kube/task/Move2KubeRetrieveTest.java
+++ workflow-examples/src/test/java/com/redhat/parodos/examples/move2kube/task/Move2KubeRetrieveTest.java
@@ -22,7 +22,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
-import static org.powermock.api.mockito.PowerMockito.when;
+import static org.mockito.Mockito.when;
 
 @Slf4j
 public class Move2KubeRetrieveTest {
