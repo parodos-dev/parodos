7ceed90ca9500bc93c66a4153398dc26de5c9685
Gloria Ciavarrini <gciavarrini@redhat.com>
2023-06-14 13:08:40 +0200

    Add Move2KubeUtils
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>

---
+++ workflow-examples/src/main/java/com/redhat/parodos/examples/move2kube/utils/Move2KubeUtils.java
@@ -0,0 +1,21 @@
+package com.redhat.parodos.examples.move2kube.utils;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * @author Gloria Ciavarrini (Github: gciavarrini)
+ */
+public abstract class Move2KubeUtils {
+
+	private Move2KubeUtils() {
+	}
+
+	public static String getPath(String server, String workspaceID, String projectID, String outputID)
+			throws URISyntaxException {
+		String path = String.format("/workspaces/%s/projects/%s/outputs/%s", workspaceID, projectID, outputID);
+		URI baseUri = new URI(server);
+		return new URI(baseUri.getScheme(), baseUri.getAuthority(), path, null, null).getPath();
+	}
+
+}
