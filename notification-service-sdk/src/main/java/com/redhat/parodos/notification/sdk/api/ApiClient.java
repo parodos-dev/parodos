53713cf09fdacd9e9741943554226a9c7b005758
Piotr Kliczewski <piotr.kliczewski@gmail.com>
2023-06-19 22:00:11 +0200

    Version bump to 1.0.18-SNAPSHOT

--- notification-service-sdk/src/main/java/com/redhat/parodos/notification/sdk/api/ApiClient.java
+++ notification-service-sdk/src/main/java/com/redhat/parodos/notification/sdk/api/ApiClient.java
@@ -143,7 +143,7 @@ public class ApiClient {
 		json = new JSON();
 
 		// Set default User-Agent.
-		setUserAgent("OpenAPI-Generator/1.0.17/java");
+		setUserAgent("OpenAPI-Generator/1.0.18-SNAPSHOT/java");
 
 		authentications = new HashMap<String, Authentication>();
 	}
