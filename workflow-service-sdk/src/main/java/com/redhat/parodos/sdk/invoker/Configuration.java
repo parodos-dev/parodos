1e9ebeb7e2eda83d6b1452960b839bff5cde7874
Piotr Kliczewski <piotr.kliczewski@gmail.com>
2023-07-01 16:50:27 +0200

    Version bump to v1.0.19-SNAPSHOT

--- workflow-service-sdk/src/main/java/com/redhat/parodos/sdk/invoker/Configuration.java
+++ workflow-service-sdk/src/main/java/com/redhat/parodos/sdk/invoker/Configuration.java
@@ -15,7 +15,7 @@ package com.redhat.parodos.sdk.invoker;
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class Configuration {
 
-	public static final String VERSION = "1.0.18";
+	public static final String VERSION = "1.0.19-SNAPSHOT";
 
 	private static ApiClient defaultApiClient = new ApiClient();
 
