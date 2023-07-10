01df03e1382d184de6c75f5beb579b5aaf4663fe
Gloria Ciavarrini <gciavarrini@redhat.com>
2023-07-10 18:25:58 +0200

    Improve javadoc for `getTaskUnderTest()`
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>

--- workflow-examples/src/test/java/com/redhat/parodos/examples/base/BaseWorkFlowCheckerTaskTest.java
+++ workflow-examples/src/test/java/com/redhat/parodos/examples/base/BaseWorkFlowCheckerTaskTest.java
@@ -23,8 +23,35 @@ public abstract class BaseWorkFlowCheckerTaskTest {
 	}
 
 	/**
-	 * @return new instance of non-abstract class extending BaseWorkFlowCheckerTaskTest
-	 */
+	 * Provides a real implementation of the class to be tested. This method should be
+	 * used when extending {@link BaseWorkFlowCheckerTaskTest} to create a test class,
+	 * such as
+	 * {@link com.redhat.parodos.examples.complex.checker.NamespaceApprovalWorkFlowCheckerTaskTest}.
+	 * Example usage: <pre> {@code
+	 *     private MyTaskClass myTaskClass;
+	 *
+	 *     &#64;Before
+	 *     public void setUp() {
+	 *         this.myTaskClass = spy((MyTaskClass) getTaskUnderTest());
+	 *         try {
+	 *              // Set up a mocked return value for the 'getRequiredParameterValue' method
+	 * 				doReturn("a_value").when(this.myTaskClass).getRequiredParameterValue(eq("a_param"));
+	 *         }
+	 * 		   catch (MissingParameterException e) {
+	 * 			throw new RuntimeException(e);
+	 *        }
+	 *     }
+	 *
+	 *     &#64;Test
+	 *     public void exampleTest() {
+	 *         // Test implementation here
+	 *         // ...
+	 *
+	 *         // Invoke the real implementation of 'checkWorkFlowStatus' method
+	 *         WorkReport workReport = myTaskClass.checkWorkFlowStatus(workContext);
+	 *     }
+	 * }</pre>
+	 **/
 	protected abstract BaseWorkFlowCheckerTask getTaskUnderTest();
 
 	public WorkFlowTaskType getType() {
